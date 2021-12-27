0f3e1be77a31a1cbecb08b73a265be84ce6cf31b
lameck onyari <lamecknyakweba3@gmail.com>
2021-12-28 01:09:09 +0300

    FIRST COMMIT

---
+++ footer.php
@@ -0,0 +1,8 @@
+ <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Student Management System
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy;Lamsoft Company  2021.</strong> All rights reserved.
+  </footer>
